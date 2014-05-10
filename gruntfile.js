544ff325dde01bf916f5d17e0a630b3ad72981f8
ricardo <ricardo.depena@gmail.com>
2014-05-09 21:48:27 -0400

    added editorconfig and jsbeautifier

--- gruntfile.js
+++ gruntfile.js
@@ -1,35 +1,58 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		watch: {
-			files: ['lib/**'],
-			tasks: ['jshint', 'nodeunit']
-		},
-		jshint: {
-			// define the files to lint
-			files: ['gruntfile.js',
-				'lib/*.js',
-				'lib/arduino/*.js',
-				'lib/rand/*.js',
-				'lib/record/*.js',
-				'lib/leap/*.js',],
-			// configure JSHint (documented at http://www.jshint.com/docs/)
-			options: {
-				// more options here if you want to override JSHint defaults
-				globals: {
-					console: true,
-					module: true
-				}
-			}
-		},
-		nodeunit: {
-			all: ['lib/test/*.js']
-		}
-	});
+    var files = ['gruntfile.js',
+        'lib/**/*.js'
+    ];
+    grunt.initConfig({
+        watch: {
+            files: files,
+            tasks: ['default']
+        },
+        jshint: {
+            // define the files to lint
+            files: files,
+            // configure JSHint (documented at http://www.jshint.com/docs/)
+            options: {
+                // more options here if you want to override JSHint defaults
+                globals: {
+                    console: true,
+                    module: true
+                }
+            }
+        },
+        nodeunit: {
+            all: ['lib/test/*.js']
+        },
+        jsbeautifier: {
+            files: files,
+            options: {
+                js: {
+                    braceStyle: "collapse",
+                    breakChainedMethods: false,
+                    e4x: false,
+                    evalCode: false,
+                    indentChar: " ",
+                    indentLevel: 0,
+                    indentSize: 4,
+                    indentWithTabs: false,
+                    jslintHappy: false,
+                    keepArrayIndentation: false,
+                    keepFunctionIndentation: false,
+                    maxPreserveNewlines: 10,
+                    preserveNewlines: true,
+                    spaceBeforeConditional: true,
+                    spaceInParen: false,
+                    unescapeStrings: false,
+                    wrapLineLength: 0
+                }
+            }
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-jsbeautifier');
 
-	grunt.registerTask('test', ['jshint', 'nodeunit']);
-	grunt.registerTask('default', ['jshint', 'nodeunit']);
+    grunt.registerTask('test', ['jshint', 'nodeunit']);
+    grunt.registerTask('default', ['jshint', 'nodeunit', 'jsbeautifier']);
 };
